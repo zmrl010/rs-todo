5cfa9b28d04ef87b5b454dd929e721b1f1edeb9a
Zach <zmriel@gmail.com>
2022-10-17 12:28:17 -0400

    chore: rearrange and cleanup cli module

--- src/cli.rs
+++ src/cli.rs
@@ -7,31 +7,35 @@ use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
 
-#[derive(Subcommand, Debug)]
 /// Commands for [`crate::tasks::Task`]
+#[derive(Subcommand, Debug)]
 pub enum TaskCommand {
     /// Add a task to the todo list
     Add {
-        #[arg()]
         /// Task description text
+        #[arg()]
         text: String,
     },
-    /// Remove a task from the todo list
+
+    /// Mark a task as complete
     Done {
+        /// Index position of task to complete
         #[arg()]
         position: usize,
     },
-    /// List all tasks in a todo list
+
+    /// List all tasks in todo list
     List,
 }
 
+/// rs-todo - a simple task manager
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
-/// Command-line interface for rs-todo
 pub struct CommandLineArgs {
     /// task subcommand to act on active list
     #[command(subcommand)]
     pub command: TaskCommand,
+
     /// data directory where application state is stored
     #[arg(short, long, env)]
     pub data_dir: Option<PathBuf>,
